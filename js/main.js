0cc008e047b1212485b05e6a5e621250ee8c97a1
nedmorgan <nmorgan1987@gmail.com>
2018-11-13 21:25:37 -0500

    Changed my web app to a fetch

--- js/main.js
+++ js/main.js
@@ -1,13 +1,37 @@
-const request = require('request');
+import config from 'config.js'
 
-var apikey = config.MY_KEY;
-let zip = 30310;
-let url = `api.openweathermap.org/data/2.5/weather?zip=${zip},us&appid=${apikey}`
+const apikey = config.MY_KEY;
+let zip = document.getElementById('zip');
+const url = `api.openweathermap.org/data/2.5/weather?zip=${zip},us&units=imperial&appid=${apikey}`;
+const ul = document.getElementById('weatherItems');
 
-request(url, function(err, response, body) {
-  if (err) {
-    console.log('error:', err);
-  } else {
-    console.log('body:', body);
-  }
-});
+//Create an element
+function createNode(element) {
+  return document.createElement(element);
+}
+
+//Append that element to the parent
+function append(parent, el) {
+  return parent.childElement(el);
+}
+
+let search = () => {
+  fetch(url) //Fetch API data from url
+    .then((resp) => resp.json()) //Transform reqeust into JSON
+    .then(function(data) {
+      let weather = data.results;
+      return weather.map(function(weather) {
+        let li = createNode('li');
+        let span = createNode('span');
+        span.innerHTML = `You searched for ${weather.name}. Currently the temperature
+      is ${weather.main.temp}. Outside it is ${weather.weather.description}.`;
+        append(li, span);
+        append(ul, li);
+      })
+    })
+
+    .catch(function(error) {
+      console.log(error);
+    });
+
+}
