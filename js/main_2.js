1ec5f10971a9aacb780d9607d3ff672451b01ae4
nedmorgan <nmorgan1987@gmail.com>
2018-11-29 18:06:44 -0500

    got my first API response for my web app

--- js/main_2.js
+++ js/main_2.js
@@ -1,17 +1,21 @@
 $(document).ready(function() {
   console.log("ready");
 
-  $("#submit").click(function(e) {
-    $.ajax({
-      type: "GET",
-      url: "http://api.openweathermap.org/data/2.5/weather?zip=30310,us&units=imperial&appid=6c0feaa89e60519b02e1df2db7c20768",
-      dataType: "json",
-      success: function (result, status, xhr) {
-        console.log("success");
-      },
-      error: function (xhr, status, error) {
-        console.log("Error: " + status + " " + error + " " + xhr.status + " " + xhr.statusText)
-      }
-    });
+  $("#submit").click(function() {
+    let zip = $("#zip").val();
+    if (zip !== "") {
+      $.ajax({
+        type: "GET",
+        url: "http://api.openweathermap.org/data/2.5/weather?zip=" + zip + ",us&units=imperial&appid=6c0feaa89e60519b02e1df2db7c20768",
+        dataType: "json",
+        success: function(wallOfText) {
+          let city = wallOfText.name;
+          $("#result").text(city);
+        },
+        error: function(xhr, status, error) {
+          console.log("Error: " + status + " " + error + " " + xhr.status + " " + xhr.statusText)
+        }
+      });
+    }
   });
 });
