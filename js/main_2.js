4fff11491aaf4c5e32bee96a17012175ed5d182f
nedmorgan <nmorgan1987@gmail.com>
2019-02-19 12:24:13 -0500

    fixed problem with hidden variable not showing

--- js/main_2.js
+++ js/main_2.js
@@ -1,14 +1,14 @@
-$(document).ready(function() {
+$(document).ready(function () {
   console.log("ready");
 
-  $("#submit").click(function() {
+  $("#submit").click(function () {
     let zip = $("#zip").val();
     if (zip !== "") {
       $.ajax({
         type: "GET",
-        url: "http://api.openweathermap.org/data/2.5/weather?zip=" + zip + ",us&units=imperial&appid=6c0feaa89e60519b02e1df2db7c20768",
+        url: `http://api.openweathermap.org/data/2.5/weather?zip=${zip},us&units=imperial&appid=${config.myKey}`,
         dataType: "json",
-        success: function(wallOfText) {
+        success: function (wallOfText) {
           let city = wallOfText.name;
           let temp = wallOfText.main.temp.toFixed(0);
           let conditions = wallOfText.weather[0].description;
@@ -19,13 +19,13 @@ $(document).ready(function() {
           $("#conditions").text("Current Conditions: " + conditions.charAt(0).toUpperCase() + conditions.substr(1).toLowerCase());
           $("#humidity").text("Humidity: " + humidity + "%");
         },
-        error: function(xhr, status, error) {
+        error: function (xhr, status, error) {
           console.log("Error: " + status + " " + error + " " + xhr.status + " " + xhr.statusText)
         }
       });
     }
   });
-  $("#reset").click(function() {
+  $("#reset").click(function () {
     $("#result-list").hide();
     $("#zip").val("");
   });
